f402a46e84282ec8b0c3c084beeb6efe4e4cce05
mehdi_SSS <shafieialavim@vcu.edu>
2015-06-23 10:04:38 -0400

    adding the third temperature and the second function

--- src/temperature.py
+++ src/temperature.py
@@ -7,5 +7,11 @@ def f_to_k(temp):
     converted=((temp-32)*(5/9.0))+273.15
     return converted
 
+
+def f_to_c(temp):
+    return ((temp-32)*(5/9.0))
+
+
 print(f_to_k(32))
 print(f_to_k(212))
+print(f_to_k(65))
